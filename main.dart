a77c800c7dbdac8a19809e30e9d47d4d15ee0baa
AnujGadekar1 <anuj.work11@gmail.com>
2025-06-09 16:30:49 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -12,12 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quick Scan to Text',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: const SplashScreen(),
+      title: 'Scan to Text',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
